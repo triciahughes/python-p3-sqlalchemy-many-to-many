ffc308753a34f362b21600a50dbf28e745b8e0b0
triciahughes <hughes.a.tricia@gmail.com>
2023-03-22 18:12:04 -0700

    complete

--- lib/models.py
+++ lib/models.py
@@ -3,12 +3,17 @@ from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime, Met
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+engine = create_engine('sqlite:///many_to_many.db')
 
-Base = declarative_base(metadata=metadata)
+Base = declarative_base()
+
+game_user = Table(
+    'game_users',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    extend_existing=True,
+)
 
 class Game(Base):
     __tablename__ = 'games'
@@ -19,6 +24,7 @@ class Game(Base):
     platform = Column(String())
     price = Column(Integer())
 
+    users = relationship('User', secondary=game_user, back_populates='games')
     reviews = relationship('Review', backref=backref('game'))
 
     def __repr__(self):
@@ -26,14 +32,32 @@ class Game(Base):
             f'title={self.title}, ' + \
             f'platform={self.platform})'
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    games = relationship('Game', secondary=game_user, back_populates='users')
+    reviews = relationship('Review', backref=backref('user'))
+
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
+
 class Review(Base):
     __tablename__ = 'reviews'
 
     id = Column(Integer(), primary_key=True)
+
     score = Column(Integer())
     comment = Column(String())
-    
+
     game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
+
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
